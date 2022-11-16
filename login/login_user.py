479d703d98f5e4cfceb324ae4fa2eb1f0139689c
Bogdan <mbogdan.fometescu@gmail.com>
2022-11-16 19:59:52 +0200

    Modify : used hashlib to encrypt pass in database

--- login/login_user.py
+++ login/login_user.py
@@ -1,14 +1,13 @@
 import sqlite3
 import os
+import hashlib
 
 
 class RegisterAndLogin:
     def __init__(self):
-        # Define the database columns
         self.column_user = "User"
         self.column_pass = "Password"
         self.column_email = "Email"
-        # Connect to the sqlite database
         try:
             self.filename = str(input("Enter name of db:"))
             self.db_path = os.getcwd()
@@ -21,8 +20,6 @@ class RegisterAndLogin:
             print("Exception class is: ", err.__class__)
         self.create_table()
 
-    """A method which will create a table in sqlite3 database"""
-
     def create_table(self):
         try:
             table = f"CREATE TABLE IF NOT EXISTS {self.filename} ({self.column_user} TEXT ,{self.column_pass} TEXT ," \
@@ -34,8 +31,6 @@ class RegisterAndLogin:
             print('Sql error: %s' % (' '.join(err.args)))
             print("Exception class is: ", err.__class__)
 
-    """A method which allows a new use registration and saves the data into the database"""
-
     def register_user(self):
         temp_list_info = []
 
@@ -44,9 +39,10 @@ class RegisterAndLogin:
             username = str(input("Choose a Username:"))
             password = str(input("Choose a Password:"))
             email = str(input("Enter your email:"))
+            hashed = hashlib.md5(password.encode()).hexdigest()
             res = self.cursor.execute(f"SELECT User FROM test")  # this needs to have a list as second argument
             user_list = res.fetchall()
-            # Refactor the list resulted from the sqlite query, so it can be used further
+
             temp_list = [item for item in map(list, user_list)]
             final_list = sum(temp_list, [])
 
@@ -57,7 +53,7 @@ class RegisterAndLogin:
 
             else:
                 temp_list_info.append(username)
-                temp_list_info.append(password)
+                temp_list_info.append(hashed)
                 temp_list_info.append(email)
 
             try:
@@ -72,23 +68,21 @@ class RegisterAndLogin:
         except ValueError:
             print("Invalid input!")
 
-    """A method which will allow the user to login based on the info stored in the database"""
-
     def login_user(self):
         username = str(input("Enter your Username:"))
         password = str(input("Enter your Password:"))
-
+        hashed = hashlib.md5(password.encode()).hexdigest()
         res = self.cursor.execute(f"SELECT User,Password FROM {self.filename}  ")
         users_list = res.fetchall()
-        # Refactor the list resulted from the sqlite query, so it can be used further
         temp_list = [item for item in map(list, users_list)]
         final_list = sum(temp_list, [])
         while True:
-            if username and password in final_list:
+            if username and hashed in final_list:
                 print("Logged in")
 
                 break
             else:
                 print("Wrong username of password")
                 username = str(input("Enter your Username:"))
-                password = str(input("Enter your Password:"))
+                hashed = str(input("Enter your Password:"))
+
